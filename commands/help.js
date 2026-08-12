b8e3216af94cacb7791239f15b2dbadc2118dbd7
kageyamahikaru481-collab <kageyamahikaru481@gmail.com>
2026-08-12 19:42:34 +0800

    Add help command

---
+++ commands/help.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: "help",
+  aliases: ["h", "commands"],
+  async execute(message, { api, config }) {
+    return api.sendMessage(
+      `🤖 ${config.botName}\n\n${config.prefix}help - commands\n${config.prefix}ping - status\n${config.prefix}uid - your UID\n${config.prefix}say <text> - repeat text\n${config.prefix}time - Philippine time`,
+      message.threadID
+    );
+  }
+};

3b5505746ef962eacf5181ff866076ff2eca5b70
kageyamahikaru481-collab <kageyamahikaru481@gmail.com>
2026-08-12 20:16:54 +0800

    Add ChatGPT command to help menu

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ module.exports = {
   aliases: ["h", "commands"],
   async execute(message, { api, config }) {
     return api.sendMessage(
-      `🤖 ${config.botName}\n\n${config.prefix}help - commands\n${config.prefix}ping - status\n${config.prefix}uid - your UID\n${config.prefix}say <text> - repeat text\n${config.prefix}time - Philippine time`,
+      `🤖 ${config.botName}\n\n${config.prefix}help - commands\n${config.prefix}ping - status\n${config.prefix}uid - your UID\n${config.prefix}say <text> - repeat text\n${config.prefix}time - Philippine time\n${config.prefix}chatgpt <tanong> - AI assistant`,
       message.threadID
     );
   }
